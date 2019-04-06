139c7edf6fbddf382ff75f0288e060c2eaa8ce93
zombieleet <zombieleetnca@gmail.com>
2019-04-06 18:36:34 +0100

    add password, fullName, healthcareFacilityId, email field

--- models/HealthFacility.js
+++ models/HealthFacility.js
@@ -3,10 +3,27 @@
 const mongoose = require("mongoose");
 
 const healthFacilitySchema = new mongoose.Schema({
+
+    password : mongoose.Schema.Types.String,
+    fullName : mongoose.Schema.Types.String,
+
+    healthFacilityId  : {
+        type: mongoose.Schema.Types.String,
+        unique: true
+    },
+
+    email: {
+        type: mongoose.Schema.Types.String,
+        unique: true
+    },
+
     hospitalName: mongoose.Schema.Types.String,
+
     location: {
         state : mongoose.Schema.Types.String,
         lga   : mongoose.Schema.Types.String,
         zone  : mongoose.Schema.Types.String
     }
 });
+
+module.exports = mongoose.model("HealthFacility", healthFacilitySchema);
