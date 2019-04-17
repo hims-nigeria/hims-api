432af382328c00f6ae1df6b737df935db5facd63
zombieleet <zombieleetnca@gmail.com>
2019-04-17 02:11:43 +0100

    implement loading of admin dashboard

--- routes/getDashboard.js
+++ routes/getDashboard.js
@@ -3,9 +3,35 @@
 const express   = require("express");
 const dashboard = express.Router();
 
-dashboard.get("/" , (req,res) => {
-    console.log("hisss");
-    res.end();
+const model = require("../models/");
+const util = require("../utils/util.js");
+
+dashboard.get("/" , async (req,res,next) => {
+
+    const { fullName , role , healthFacilityId } = req.session.user;
+    console.log(healthFacilityId);
+    try {
+
+        const data = {};
+
+        switch( role ) {
+        case "admin":
+            Object.assign(data,{
+                status: 200,
+                message: {
+                    dashboardInfo: (await model.healthFacility.findOne( { healthFacilityId }))._doc.dashboardInfo,
+                    fullName,
+                    role
+                }
+            });
+            break;
+        }
+        return res.status(200).json(data);
+
+    } catch(ex) {
+        return next(ex);
+    }
+
 });
 
 module.exports = dashboard;
